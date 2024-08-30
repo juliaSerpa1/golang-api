db51e219562d92cd5ad7aff2d6872d27ca1b8112
juliaSerpa1 <julia_jmsn@hotmail.com>
2024-08-30 18:10:25 -0300

    finalizando testes automatizados unitarios

--- router/router.go
+++ router/router.go
@@ -1,19 +1,48 @@
 package router
 
 import (
-    "github.com/gin-gonic/gin"
-    "golang-api/controller"
-    "golang-api/di"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"golang-api/controller"
+	"golang-api/infra/db"
+	"golang-api/infra/jwt"
+    "golang-api/mocks/db"
+    "golang-api/mocks/jwt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
-func SetupRoutes(container *di.Container) *gin.Engine {
-    r := gin.Default()
+type MockContainer struct {
+    JWTService jwt.JWTService
+    DBService  db.DBService
+}
 
-    authController := controller.NewAuthController(container.JWTService)
-    userController := controller.NewUserController(container.DBService)
+func SetupRoutes(jwtService jwt.JWTService, dbService db.DBService) *gin.Engine {
+    router := gin.Default()
+    authController := controller.NewAuthController(jwtService)
+    userController := controller.NewUserController(dbService)
 
-    r.POST("/login", authController.Login)
-    r.GET("/user/:id", userController.GetUser)
+    router.POST("/login", authController.Login)
+    router.GET("/user/:id", userController.GetUser)
+    return router
+}
 
-    return r
+func TestSetupRoutes(t *testing.T) {
+    mockJWTService := mockjwt.NewMockJWTService()
+    mockDBService := mockdb.NewMockDBService()
+
+    router := SetupRoutes(mockJWTService, mockDBService)
+
+    req, _ := http.NewRequest("POST", "/login", nil)
+    w := httptest.NewRecorder()
+    router.ServeHTTP(w, req)
+    assert.Equal(t, http.StatusOK, w.Code)
+
+    req, _ = http.NewRequest("GET", "/user/123", nil)
+    w = httptest.NewRecorder()
+    router.ServeHTTP(w, req)
+    assert.Equal(t, http.StatusOK, w.Code)
 }
